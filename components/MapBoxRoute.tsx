7a81ba730e7fd8484ded1f29e813250447f5af8e
Ritik Kumar Pathak <phritik06@gmail.com>
2024-04-12 13:07:47 +0530

    production error solved

--- components/MapBoxRoute.tsx
+++ components/MapBoxRoute.tsx
@@ -1,15 +1,25 @@
+
 import React from "react";
 import { Layer, Source } from "react-map-gl";
+import { Feature, GeoJsonProperties, Geometry } from "geojson";
 
-const MapBoxRoute = (props: any) => {
+// Define a type for GeoJSON data
+interface GeoJSONData extends Feature<Geometry, GeoJsonProperties> {
+  geometry: {
+    type: "LineString";
+    coordinates: number[][];
+  };
+}
+
+const MapBoxRoute = (props: { coordinates: number[][] }) => {
+  const geojsonData: GeoJSONData = {
+    type: "Feature",
+    geometry: { type: "LineString", coordinates: props.coordinates },
+    properties: {},
+  };
+
   return (
-    <Source
-      type="geojson"
-      data={{
-        type: "Feature",
-        geometry: { type: "LineString", coordinates: props.coordinates },
-      }}
-    >
+    <Source type="geojson" data={geojsonData}>
       <Layer
         type="line"
         layout={{ "line-join": "round", "line-cap": "square" }}
@@ -20,3 +30,4 @@ const MapBoxRoute = (props: any) => {
 };
 
 export default MapBoxRoute;
+
